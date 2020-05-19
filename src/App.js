7a94de8729cf606c9c07737c6b22a1d0b0c0c791
Raka <56627679+rakaadi@users.noreply.github.com>
2020-05-19 23:25:23 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      string: "Our first react-start."
+      monsters: [
+        
+      ]
     }
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={() => this.setState({string: "Hello Raka!"})}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(
+            monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
       </div>
     );
   }
