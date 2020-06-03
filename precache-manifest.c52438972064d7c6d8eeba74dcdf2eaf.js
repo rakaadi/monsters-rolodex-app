7e1b39653bfc4f5d50f7b7ca4f63a087cecec04f
Raka <56627679+rakaadi@users.noreply.github.com>
2020-06-03 14:35:30 +0700

    Updates

---
+++ precache-manifest.c52438972064d7c6d8eeba74dcdf2eaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3c9e605b8d4b1d9732968cdf791aba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56aad2903a097ee65b7a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d89edd4f815e94ef344c",
+    "url": "/static/js/2.9ed52bd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ed52bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56aad2903a097ee65b7a",
+    "url": "/static/js/main.d540f20a.chunk.js"
+  },
+  {
+    "revision": "7129a994307f4aa325e4",
+    "url": "/static/js/runtime-main.c8f32266.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
